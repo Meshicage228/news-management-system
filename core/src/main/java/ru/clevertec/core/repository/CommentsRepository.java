47469552da3a05cbfd8894ccffad6e3798ffb031
Vlad.Zherko <vladikon19@tut.by>
2024-10-30 00:07:36 +0300

    specification and page support

--- core/src/main/java/ru/clevertec/core/repository/CommentsRepository.java
+++ core/src/main/java/ru/clevertec/core/repository/CommentsRepository.java
@@ -1,9 +1,14 @@
 package ru.clevertec.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.core.entity.CommentEntity;
 
 @Repository
-public interface CommentsRepository extends JpaRepository<CommentEntity, Long> {
+public interface CommentsRepository extends JpaRepository<CommentEntity, Long>,
+                                            JpaSpecificationExecutor<CommentEntity>,
+                                            PagingAndSortingRepository<CommentEntity, Long> {
+
 }
--- core/src/main/java/ru/clevertec/core/repository/NewsRepository.java
+++ core/src/main/java/ru/clevertec/core/repository/NewsRepository.java
@@ -1,9 +1,13 @@
 package ru.clevertec.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.core.entity.NewsEntity;
 
 @Repository
-public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
+public interface NewsRepository extends JpaRepository<NewsEntity, Long>,
+                                        JpaSpecificationExecutor<NewsEntity>,
+                                        PagingAndSortingRepository<NewsEntity, Long> {
 }
