7d8a02bb0607bace4d7db43f38a1638af9b0958a
Vlad.Zherko <vladikon19@tut.by>
2024-10-30 00:06:07 +0300

    log aop init

---
+++ api/src/main/java/ru/clevertec/api/aspect/LogAspect.java
@@ -0,0 +1,31 @@
+package ru.clevertec.api.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAspect {
+    @Pointcut("within(@ru.clevertec.api.aspect.LogRequestResponse *) && execution(* *(..))")
+    public void annotatedByLoggableWithDuration() {}
+
+    @Around("annotatedByLoggableWithDuration()")
+    public Object logPointcut(ProceedingJoinPoint joinPoint) {
+        log.info("Entering method {} with arguments : {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+        // todo exception
+        Object proceed = null;
+        try {
+            proceed = joinPoint.proceed();
+            log.info("Exiting method {} with response : {}", joinPoint.getSignature().getName(), proceed);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        return proceed;
+    }
+}
