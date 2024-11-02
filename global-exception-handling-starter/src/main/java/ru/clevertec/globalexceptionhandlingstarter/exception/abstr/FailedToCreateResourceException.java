17f6f39af99b928667d00b02373af10a4b4b030e
Vlad.Zherko <vladikon19@tut.by>
2024-11-02 15:47:16 +0300

    removed

--- global-exception-handling-starter/src/main/java/ru/clevertec/globalexceptionhandlingstarter/exception/abstr/FailedToCreateResourceException.java
+++
@@ -1,7 +0,0 @@
-package ru.clevertec.globalexceptionhandlingstarter.exception.abstr;
-
-public abstract class FailedToCreateResourceException extends RuntimeException {
-    public FailedToCreateResourceException(String message) {
-        super(message);
-    }
-}
--- global-exception-handling-starter/src/main/java/ru/clevertec/globalexceptionhandlingstarter/exception/abstr/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.clevertec.globalexceptionhandlingstarter.exception.abstr;
-
-public abstract class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
