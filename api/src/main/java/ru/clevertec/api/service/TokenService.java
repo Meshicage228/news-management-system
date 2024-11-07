b68e5a5e4d8dd76e02e0199ade7b43f6fa425049
Vlad.Zherko <vladikon19@tut.by>
2024-11-08 02:17:27 +0300

    updated : token logic

--- api/src/main/java/ru/clevertec/api/service/TokenService.java
+++ api/src/main/java/ru/clevertec/api/service/TokenService.java
@@ -1,7 +1,10 @@
 package ru.clevertec.api.service;
 
+import org.springframework.security.core.Authentication;
 import ru.clevertec.api.dto.user.UserRequestDto;
 
 public interface TokenService {
     String createToken(UserRequestDto userRequestDto);
+
+    Authentication fromToken(String token);
 }
--- api/src/main/java/ru/clevertec/api/service/impl/TokenServiceImpl.java
+++ api/src/main/java/ru/clevertec/api/service/impl/TokenServiceImpl.java
@@ -1,9 +1,14 @@
 package ru.clevertec.api.service.impl;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import ru.clevertec.api.clients.UserClient;
 import ru.clevertec.api.dto.user.UserRequestDto;
@@ -12,7 +17,9 @@ import ru.clevertec.api.service.SecretKeyGenerator;
 import ru.clevertec.api.service.TokenService;
 
 import javax.crypto.SecretKey;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,10 +43,27 @@ public class TokenServiceImpl implements TokenService {
 
         return Jwts.builder().subject(loginUser.getUsername())
                 .expiration(new Date(System.currentTimeMillis() + expiryTime))
-                .claim("username", loginUser.getUsername())
-                .claim("id", loginUser.getId())
                 .claim("role", loginUser.getRole())
+                .claim("username", loginUser.getUsername())
                 .signWith(secretKey)
                 .compact();
+    }
+
+    @Override
+    public Authentication fromToken(String token) {
+        JwtParser parser = Jwts.parser()
+                .setSigningKey(secretKey)
+                .build();
+
+        Claims payload = (Claims) parser.parse(token).getPayload();
+
+        String role = "ROLE_" + payload.get("role");
+        String username = (String) payload.get("username");
+
+        List<SimpleGrantedAuthority> list = Arrays.stream(role.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        return new UsernamePasswordAuthenticationToken(username, null, list);
     }
 }
